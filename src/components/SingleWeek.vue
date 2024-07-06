08dbcb6762bef3ce24c26ea53dc50b046f3a81cd
Tom <36952502+tcripps25@users.noreply.github.com>
2024-07-06 17:59:48 +0100

    update

--- src/components/SingleWeek.vue
+++ src/components/SingleWeek.vue
@@ -79,7 +79,7 @@ const toggleEditTitle = () => {
         </template>
         <div class="flex justify-center items-center gap-2">
           <Button @click="addActivity(index)"
-            pt:root:class="bg-sky-600 hover:bg-sky-500 text-sm text-sky-50 px-3 py-2 rounded-md flex items-center gap-1 w-max">
+            pt:root:class="bg-slate-100 hover:bg-slate-200 transition text-sm text-sky-600 border px-3 py-2 rounded-md flex items-center gap-1 w-max">
             Add
             Activity
             <PlusCircleIcon class="w-5 h-5" />
