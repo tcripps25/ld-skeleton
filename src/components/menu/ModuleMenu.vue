8d7eb7f96e4e63a8b057032a681712327f0fa68f
Tom <36952502+tcripps25@users.noreply.github.com>
2024-07-24 17:27:37 +0100

    update

--- src/components/menu/ModuleMenu.vue
+++ src/components/menu/ModuleMenu.vue
@@ -22,11 +22,11 @@ const moduleData = computed(() => {
 
 <template>
     <Menu unstyled :model="moduleData" pt:submenuLabel="font-medium text-sm text-slate-500 mt-1 mb-1"
-        pt:root="before:-top-1 before:bottom-[2.3rem] before:content-[''] before:rounded before:w-[3px] before:-left-4 before:-mb-5 before:bg-slate-200 before:absolute">
+        pt:root="before:-top-0 before:bottom-[2.2rem] before:content-[''] before:rounded before:w-[2px] before:-left-4 before:-mb-5 before:bg-slate-300 before:absolute">
         <template #item="{ item, props }">
             <RouterLink v-if="item.route" v-slot="{ href, navigate }" :to="item.route"
                 active-class="bg-slate-200 text-slate-600 hover:!bg-slate-200 font-semibold"
-                class="relative transition hover:bg-slate-200 gap-2 flex p-2 rounded before:rounded-sm items-center before:content-[''] before:absolute before:-left-4  before:w-3 before:h-0 before:border-b-[3px] before:border-l-[3px] before:top-4 before:bottom-8">
+                class="relative transition hover:bg-slate-200 gap-2 flex p-2 rounded before:rounded-sm items-center before:content-[''] before:absolute before:-left-4  before:w-3 before:h-0 before:border-b-[2px] before:border-l-[2px] before:top-5 before:bottom-8 before:border-slate-300">
                 <div class="flex items-center">
                     <span class="">{{ item.label }}</span>
                 </div>
