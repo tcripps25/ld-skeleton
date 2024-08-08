d14f84da84d76412dc2af9e0d7a238b2b37aa7f9
Tom <36952502+tcripps25@users.noreply.github.com>
2024-08-08 17:44:17 +0100

    update

--- src/components/ActivityPlaceholder.vue
+++ src/components/ActivityPlaceholder.vue
@@ -10,9 +10,8 @@ const props = defineProps({
         <p class="bg-slate-100 rounded-lg flex justify-center items-center p-10" v-if="(activities.length < 1)">No
             Activities have been added to this week yet.</p>
         <div v-else class="grid lg:grid-cols-2 xl:grid-cols-3 md:grid-cols-1 gap-5">
-            <div v-for="(activity, index) in activities"
-                class="p-10 w-full bg-blue-200 rounded flex justify-center items-center">
-                <p>{{ activity.title }}</p>
+            <div v-for="(activity, index) in activities" class="p-5 h-44 w-full bg-sky-500 rounded-lg flex shadow">
+                <h4 class="text-sky-50 font-medium text-xl">{{ activity.title }}</h4>
             </div>
         </div>
     </div>
